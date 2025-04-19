b5caed7baa2d398c7929b05f21e9d48fee40f91e
Youu460 <nisanox34@gmail.com>
2025-04-19 18:05:14 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/Leomc_bot'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/Leomc_bot')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/+28JskRlPHb5lOWQ0')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
